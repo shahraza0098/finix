37f040de206881e9ccefe575c33e90830d0d23ba
Shahid Raza <engineerbro0098@gmail.com>
2025-07-12 10:57:41 +0530

    set layout

--- src/app/(HomePage)/home/page.jsx
+++ src/app/(HomePage)/home/page.jsx
@@ -1,12 +1,12 @@
 // src/app/page.tsx
-import Header from "../_components/Header";
+//import Header from "../_components/Header";
 import Hero from "../_components/HeroSec";
 import Features from "../_components/Features";
 
 export default function HomePage() {
   return (
     <main className="relative">
-      <Header/>
+      {/* <Header/> */}
       <Hero />
       <Features />
     </main>
